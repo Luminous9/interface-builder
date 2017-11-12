0ae9bcba1d2e8e8ac52c913de86ec39dbbaa1340
Sonny Zhang <szhang@nascentdigital.com>
2017-11-12 14:18:12 -0500

    added basic user model

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,37 +1,19 @@
 import React, { Component } from 'react'
-import { getApiVersion, getInterfaces } from '../../client'
 import './app.css'
 
 class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      text: 'Not Connected',
-      interfaces: []
-    }
-  }
-
-  componentDidMount() {
-    getApiVersion().then(res => {
-      this.setState({ text: res.message })
-    })
-
-    getInterfaces().then(res => {
-      this.setState({ interfaces: res })
-    })
-  }
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     text: 'Not Connected',
+  //     interfaces: []
+  //   }
+  // }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.text}</p>
-        <p>Stored interfaces:</p>
-        {this.state.interfaces.map((gameInterface, i) => {
-          return <p key={i}>{`${gameInterface.name} by ${gameInterface.author}`}</p>
-        })}
+        <h1>Keeper</h1>
       </div>
     )
   }
